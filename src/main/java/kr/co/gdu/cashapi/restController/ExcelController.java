68a7e06a307c802511604dad5f3b71163c2b43ce
wntjd <wntjd@DESKTOP-8A08JA2>
2020-11-10 20:19:13 +0900

    [김주성] porm.xml에 apache.poi 추가하였음 view 패키지 생성 후 CashListExcel.java추가 파일을 엑셀형식으로 보여줍니다 ㅣ에 맞게 controller, service추가 하였습니다

---
+++ src/main/java/kr/co/gdu/cashapi/restController/ExcelController.java
@@ -0,0 +1,13 @@
+package kr.co.gdu.cashapi.restController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExcelController {
+	
+	@GetMapping(path ="/cashListExcel", produces = "application/vnd.ms-excel")
+	public String cashListExcel() {
+		return "cashListExcel";
+	}
+}
